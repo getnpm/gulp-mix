acd3da95a8f9ac6fa0ee7541700ddc454b44663d
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 18:51:39 +0300

    Update Mix.js

--- src/Mix.js
+++ src/Mix.js
@@ -1,9 +1,15 @@
+const gulp = require('gulp');
+
 class Mix {
 
     static _init = null;
 
     static get init() {
         return Mix._init || (Mix._init = new Mix());
+    }
+
+    static js(source, output) {
+        gulp.src(source).pipe(gulp.dest(output));
     }
 }
 
