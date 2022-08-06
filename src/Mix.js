2369dda2c38f49dc14aa9805ce47c1b142cc8b2a
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 11:07:23 +0300

    Update Mix.js

--- src/Mix.js
+++ src/Mix.js
@@ -1,19 +1,8 @@
 const gulp = require('gulp');
 
-const defaultPlugins = {
-    if: require('gulp-if'),
-    rename: require('gulp-rename'),
-    sourcemaps: require('gulp-sourcemaps')
-};
-
 class Mix {
     static js(src, output) {
-        return gulp.src(src)
-            .pipe(gulp.dest(output))
-            .pipe(defaultPlugins.rename({suffix: '.min'}))
-            .pipe(defaultPlugins.sourcemaps.init())
-            .pipe(defaultPlugins.sourcemaps.write('./'))
-            .pipe(gulp.dest(output));
+        return gulp.src(src).pipe(gulp.dest(output));
     }
 }
 
