fca6d117ddf02cedef5754df56d06f3e5b1ea0fd
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 12:16:32 +0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,5 @@
-import gulpIf from 'gulp-if'
-import gulpRename from 'gulp-rename'
-import gulpSourcemaps from 'gulp-sourcemaps'
-
 export const plugins = {
-    if: gulpIf,
-    rename: gulpRename,
-    sourcemaps: gulpSourcemaps
+    if: require('gulp-if'),
+    rename: require('gulp-rename'),
+    sourcemaps: require('gulp-sourcemaps')
 }
