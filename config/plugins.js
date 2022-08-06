ac1f072c581871af69b867755b6c91c5bbf0ad38
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 11:51:00 +0300

    Create plugins.js

---
+++ config/plugins.js
@@ -0,0 +1,9 @@
+import gulpIf from 'gulp-if'
+import gulpRename from 'gulp-rename'
+import gulpSourcemaps from 'gulp-sourcemaps'
+
+export const plugins = {
+    if: gulpIf,
+    rename: gulpRename,
+    sourcemaps: gulpSourcemaps
+}
