3aac5ba94223676524e4b1a0b03d55843094d426
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 12:06:49 +0300

    Update Mix.js

--- src/Mix.js
+++ src/Mix.js
@@ -1,11 +1,11 @@
 class Mix {
     static jsTranspile(source, output) {
-        app.gulp.src(source)
-            .pipe(app.gulp.dest(output))
-            .pipe(app.plugins.rename({suffix: '.min'}))
-            .pipe(app.plugins.sourcemaps.init())
-            .pipe(app.plugins.sourcemaps.write('./'))
-            .pipe(app.gulp.dest(output));
+        gulp.init.src(source)
+            .pipe(gulp.init.dest(output))
+            .pipe(gulp.plugins.rename({suffix: '.min'}))
+            .pipe(gulp.plugins.sourcemaps.init())
+            .pipe(gulp.plugins.sourcemaps.write('./'))
+            .pipe(gulp.init.dest(output));
     }
 }
 
