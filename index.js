48dfde4db699133f19d376ff173c071b6a5a4cc2
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 11:58:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import gulp from 'gulp'
+import {plugins} from './config/plugins'
+
+global.app = {
+    gulp: gulp,
+    plugins: plugins,
+}
+
 const mix = require('./src/Mix');
 
 module.exports = mix;
