d9816ae5b799a12a30110b307dc140490dc55450
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-08-06 12:21:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const init = require('gulp'); // Основной модуль;
-const {plugins} = require('./config/plugins'); // Плагины по-умолчанию;
-
-// Передача значений в глобальную переменную;
-global.gulp = {
-    init: init,
-    plugins: plugins,
-}
-
 const mix = require('./src/Mix');
 
 module.exports = mix;
